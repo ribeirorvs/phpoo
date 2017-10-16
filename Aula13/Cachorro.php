1af03e9fbaff92f75e35f8b8aa265fe07e86eccb
rribeiro <ribeiro.rvs@hotmail.com>
2017-10-16 03:08:29 -0200

    include exercise

--- Aula13/Cachorro.php
+++ Aula13/Cachorro.php
@@ -6,4 +6,46 @@ class Cachorro extends Lobo {
     public function emitirSom() {
         echo "<p>Au au au au</p>";
     }
+    
+    public function reagirFrase($frase){
+        if($frase == "Olá" || $frase == "Toma comida"){
+            echo "<p>Abanar e latir</p>";
+        } else {
+            echo "<p>Rosnar</p>";
+        }
+    }
+    
+    public function reagirHora($hora, $min){
+        if($hora < 12){
+            echo "<p>Abanar</p>";
+        } elseif ($hora >= 18){
+            echo "<p>Latir</p>";
+        } else {
+            echo "<p>Rosnar</p>";
+        }
+    }
+    
+    public function reagirDono($dono){
+        if($dono){
+            echo "<p>Abanar</p>";
+        } else {
+            echo "<p>Rosnar e Latir</p>";
+        }
+    }
+    
+    public function reagirIdadePeso($idade, $peso){
+        if($idade < 5 ){
+            if($peso<10){
+                echo "<p>Abanar</p>";
+            } else {
+                echo "<p>Latir</p>";
+            }
+        } else {
+            if($peso < 10){
+                echo "<p>Rosnar</p>";
+            } else {
+                echo "<p>Ignorar</p>";
+            }
+        }
+    }
 }
--- Aula13/index.php
+++ Aula13/index.php
@@ -7,7 +7,15 @@
     <body>
         <pre>
         <?php
+            require_once 'Cachorro.php';
             
+            $c = new Cachorro();
+            
+            $c->reagirDono(false);
+            $c->reagirFrase("Olá");
+            $c->emitirSom();
+            $c->reagirHora(10, 00);
+            $c->reagirIdadePeso(15, 20);
         ?>
         </pre>
     </body>
